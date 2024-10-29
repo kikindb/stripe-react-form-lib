50fa456b17549b579f674b4128ca8e099da7229a
= <=>
2024-10-29 18:36:52 -0500

    fix: pass ref in the HOC withQueryClientProvider

--- src/lib/utils/withQueryClientProvider.jsx
+++ src/lib/utils/withQueryClientProvider.jsx
@@ -1,24 +1,19 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/display-name */
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-// Initialize the Query Client
 const queryClient = new QueryClient();
 
-// Define the HOC
 const withQueryClientProvider = (WrappedComponent) => {
-  // Return a new component that wraps the WrappedComponent with QueryClientProvider
-  return function WithQueryClientProvider(props) {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <WrappedComponent {...props} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    );
-  };
+  return forwardRef((props, ref) => (
+    <QueryClientProvider client={queryClient}>
+      <WrappedComponent {...props} ref={ref} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  ));
 };
 
 export default withQueryClientProvider;
